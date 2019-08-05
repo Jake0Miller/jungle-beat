f120bfe4eedebd8ddfcc28a011a9c3758a0f833d
Jake Miller <jake0miller@gmail.com>
2019-08-04 22:24:26 -0600

    Create node class with TDD

---
+++ test/node_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+require 'pry'
+
+class NodeTest < MiniTest::Test
+  def setup
+    @node = Node.new("plop")
+  end
+
+  def test_it_exists
+    assert_instance_of Node, @node
+  end
+
+  def test_it_has_data
+    assert_equal "plop", @node.data
+  end
+
+  def test_next_default_defaults_to_nil
+    assert_nil @node.next_node
+  end
+end
